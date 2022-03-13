3c902a6b2094175975a0ee9b95d8be9fda21b187
yah <zilliz@zillizs-MacBook-Pro.local>
2022-03-13 12:38:06 +0800

    Remove Proxy now
    Signed-off-by: yah <zilliz@zillizs-MacBook-Pro.local>

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,21 +1,21 @@
-package proxy
+// package proxy
 
-import (
-	"context"
+// import (
+// 	"context"
 
-	"github.com/yah01/CyberKV/proto"
-)
+// 	"github.com/yah01/CyberKV/proto"
+// )
 
-type Proxy struct {
-	proto.UnsafeKeyValueServer
-}
+// type Proxy struct {
+// 	proto.UnsafeKeyValueServer
+// }
 
-func (proxy *Proxy) Get(context.Context, *proto.ReadRequest) (*proto.ReadResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
-}
-func (proxy *Proxy) Set(context.Context, *proto.WriteRequest) (*proto.WriteResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Set not implemented")
-}
-func (proxy *Proxy) Remove(context.Context, *proto.WriteRequest) (*proto.WriteResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Remove not implemented")
-}
+// func (proxy *Proxy) Get(context.Context, *proto.ReadRequest) (*proto.ReadResponse, error) {
+// 	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
+// }
+// func (proxy *Proxy) Set(context.Context, *proto.WriteRequest) (*proto.WriteResponse, error) {
+// 	return nil, status.Errorf(codes.Unimplemented, "method Set not implemented")
+// }
+// func (proxy *Proxy) Remove(context.Context, *proto.WriteRequest) (*proto.WriteResponse, error) {
+// 	return nil, status.Errorf(codes.Unimplemented, "method Remove not implemented")
+// }
